7d034ad3f9a9921e57d422208e675858b87f6a5f
szymon Truszkowski <szymontruszwork@gmail.com>
2023-12-01 15:00:55 +0000

    final commit

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,5 +1,5 @@
 const clientId = '478be30c1f9a48868dcc0c943137aabf';
-const redirectUri = 'https://vcjmp3f.netlify.app';
+const redirectUri = 'http://localhost:3000/callbac';
 let accessToken;
 
 const Spotify = {
