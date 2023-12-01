3d278d6aa771a65a19491966dda19e864de243c5
szymon Truszkowski <szymontruszwork@gmail.com>
2023-12-01 14:50:50 +0000

    netlify/callback/

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,5 +1,5 @@
 const clientId = '478be30c1f9a48868dcc0c943137aabf';
-const redirectUri = 'http://localhost:3000/callback/';
+const redirectUri = 'https://vcjmp3f.netlify.app/callback/';
 let accessToken;
 
 const Spotify = {
